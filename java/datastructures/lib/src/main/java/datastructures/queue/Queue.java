a31b6fbfeb1e9dd6d32d77f9adf5974655b43d65
RocioCMartinez <rym5411@gmail.com>
2023-07-24 00:52:46 -0400

    added root methods for queue class

--- java/datastructures/lib/src/main/java/datastructures/queue/Queue.java
+++ java/datastructures/lib/src/main/java/datastructures/queue/Queue.java
@@ -7,26 +7,52 @@ public class Queue<T>
   Node<T> front;
   Node<T> back;
 
-  void enqueue(T valueToEnqueue)
-  {
-    // TODO: implement me
+  void enqueue(T valueToEnqueue) {
+    // Created the node to hold the value we want to add
+    Node<T> newNode = new Node<>(valueToEnqueue);
+    // We check if the queue is empty, if so our newNode will be assigned as both front and back because it is the only value inside
+    if (isEmpty()) { // This will come into play by the end with isEmpty method
+      front = newNode;
+      back = newNode;
+    } else {
+      // We align the newNode to be after the previously last one
+      back.next = newNode;
+      // We give it the 'last' one title, newNode now equal last one
+      back = newNode;
+    }
+
+
   }
 
-  T dequeue()
-  {
-    // TODO: implement me
-    return null;
+  T dequeue() {
+    if (isEmpty()){
+      return null;
+    }
+    // Create a variable to record the value of the front node
+    T dequeuedValue = front.value;
+    // if there is only one Node, set it to null and you will get rid of the one node
+    if (front == back){
+      front = null;
+      back = null;
+    } else {
+      // Otherwise disconnect the front node
+      front = front.next;
+    }
+    //Return the value of the node that was lost/removed
+    return dequeuedValue;
   }
 
-  T peek()
-  {
-    // TODO: implement me
-    return null;
+  T peek() {
+    //Check if queue is empty
+    if (isEmpty()){
+      return null;
+    }
+    //If it is not empty start looking in by determining the value of 'front' node
+    return front.value;
   }
 
-  boolean isEmpty()
-  {
-    // TODO: implement me
-    return false;
+  boolean isEmpty() {
+    // If there is no front node, there is nothing in the queue, therefore the bottom statement would be true, otherwise false.
+    return front == null;
   }
 }
